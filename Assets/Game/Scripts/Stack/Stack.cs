1eb594db67697a69a569134fb0ddb4a465c79439
Ahmet Yılmaz <ahmetyilmaz0964@gmail.com>
2022-10-19 01:45:23 +0300

    Add Cut Stack

--- Assets/Game/Scripts/Stack/Stack.cs
+++ Assets/Game/Scripts/Stack/Stack.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Game.Scripts.Stack
@@ -5,13 +6,26 @@ namespace Game.Scripts.Stack
     public class Stack : MonoBehaviour
     {
         public float speed;
-        public Transform stackPiece ;
+        public Transform stackPiece;
+        private bool _isStop;
 
+        private void Start()
+        {
+            GameManager.Instance.onTouch.AddListener(StopMove);
+        }
 
         private void Update()
         {
-            var pos = transform.position;
-            transform.position = new Vector3(Mathf.Sin(Time.time * speed), pos.y, pos.z);
+            if (!_isStop)
+            {
+                var pos = transform.position;
+                transform.position = new Vector3(Mathf.Sin(Time.time * speed), pos.y, pos.z);
+            }
+        }
+
+        private void StopMove()
+        {
+            _isStop = true;
         }
     }
 }
--- Assets/Game/Scripts/Stack/StackController.cs
+++ Assets/Game/Scripts/Stack/StackController.cs
@@ -4,19 +4,55 @@ namespace Game.Scripts.Stack
 {
     public class StackController : MonoBehaviour
     {
-        private Stack _currentStack;
-        private void Start()
+        [SerializeField] private Stack currentStack;
+        [SerializeField] private float tolerance;
+        private Vector3 _initCurrentStackPosition;
+
+        public void ControlStep()
         {
+            if (Mathf.Abs(currentStack.transform.position.x) <= tolerance)
+            {
+                SuccesStep();
+            }
+            else
+            {
+                StackStopAndCut();
+            }
         }
 
-        private void Update()
+        private void SuccesStep()
         {
+            Debug.Log("HELLALL");
         }
 
-        public void StackStopAndCut()
+        private void StackStopAndCut()
         {
-            _currentStack.speed = 0;
-            _currentStack.stackPiece.gameObject.SetActive(true);
+            currentStack.speed = 0;
+            currentStack.stackPiece.gameObject.SetActive(true);
+            _initCurrentStackPosition = currentStack.transform.position;
+            currentStack.transform.localScale -= Vector3.right * Mathf.Abs(_initCurrentStackPosition.x);
+            currentStack.transform.position -= Vector3.right * (_initCurrentStackPosition.x / 2);
+            currentStack.stackPiece.localScale = new Vector3(
+                (currentStack.stackPiece.localScale.x / currentStack.transform.localScale.x) *
+                Mathf.Abs(currentStack.transform.position.x) * 2, currentStack.stackPiece.localScale.y,
+                currentStack.stackPiece.localScale.z);
+            if (currentStack.stackPiece.position.x > 0)
+            {
+                DetermineStackPiecePosition(1);
+            }
+            else
+            {
+                DetermineStackPiecePosition(-1);
+            }
+
+            currentStack.stackPiece.GetComponent<Rigidbody>().isKinematic = false;
+        }
+
+        private void DetermineStackPiecePosition(int coefficient)
+        {
+            currentStack.stackPiece.position += (Vector3.right *
+                                                 ((currentStack.transform.localScale.x / 2) +
+                                                  currentStack.stackPiece.lossyScale.x / 2)) * coefficient;
         }
     }
 }
